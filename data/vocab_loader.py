4e59effd42795e557edf3acfd34ae6aa432daee1
Doria-Felipe <felipefdoria@gmail.com>
2026-03-10 19:21:27 +1100

    Cleaning the repo

--- data/vocab_loader.py
+++
@@ -1,9 +0,0 @@
-import json
-from pathlib import Path
-
-VOCAB_PATH = Path("data/topics_vocab_clean.json")
-
-
-def load_vocab():
-    with open(VOCAB_PATH, encoding="utf8") as f:
-        return json.load(f)
